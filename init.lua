9f7b8cf9aa142ac5aec3bc4f943fde63fda73021
Justyn Temme <justyntemme@gmail.com>
2023-04-26 12:35:04 -0500

    better syntax highlighting for vim-go

--- init.lua
+++ init.lua
@@ -15,6 +15,18 @@ vim.cmd([[call plug#end()]])
 -- vim.call('deoplete#custom#option', 'omni_patterns', {
 --   ['go'] = '[^. *\t]\\.[[:alnum:]_]*'
 -- })
+--“ Go syntax highlighting
+vim.g.go_highlight_fields = 1
+vim.g.go_highlight_functions = 1
+vim.g.go_highlight_function_calls = 1
+vim.g.go_highlight_extra_types = 1
+vim.g.go_highlight_operators = 1
+
+-- “ Auto formatting and importin
+vim.g.go_fmt_autosave = 1
+vim.g.go_fmt_command = "goimports"
+--
+--
 vim.call('deoplete#custom#option', 'omni_patterns', {
  ['go'] = '[^. *\t]\\.\\w*',
 })
