e06974ffd6a63d51e210012e3c769690e6528746
Justyn Temme <justyntemme@gmail.com>
2024-09-05 17:09:59 -0500

    moving to pyright

--- nvim/init.lua
+++ nvim/init.lua
@@ -26,7 +26,7 @@ require("lazy").setup({
 
 spec ={ -- add your plugins here
 	{"neovim/nvim-lspconfig"}, 
-	{"nvim-treesitter/nvim-treesitter"},
+	--{"nvim-treesitter/nvim-treesitter"},
 	{ "catppuccin/nvim", 
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
     		priority = 1000, -- make sure to load this before all the other start plugins
