28a82c96b48df39cbfdd219652d10c5a933dcec6
Justyn Temme <justyntemme@gmail.com>
2024-11-04 16:39:25 -0600

    getting into nix

---
+++ flake.nix
@@ -0,0 +1,88 @@
+{
+  description = "Darwin-laptop-eatch";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ pkgs.vim
+          ];
+
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+      security.pam.enableSudoTouchIdAuth = true;
+
+
+      system.defaults = {
+          dock.autohide = false;
+          finder.AppleShowAllExtensions = true;
+      };
+      
+
+      homebrew.enable = true;
+      homebrew.casks = [
+          "cocoapods"
+          "visual-studio-code"
+          "postman"
+          "android-platform-tools"
+        ];
+
+      homebrew.brews = [
+            "zsh-autosuggestions"
+            "tailscale"
+            "imagemagick"
+            "rust"
+            "sqlite"
+            "python"
+            "gnupg"
+            "go"
+            "gcc"
+            "node"
+            "llvm"
+            "neovim"
+            "cmake"
+            "tree-sitter"
+            "zsh"
+          
+        ];
+
+    };
+
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#earth
+    darwinConfigurations."earth" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+      specialArgs = { inherit inputs; };
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."earth".pkgs;
+  };
+}
