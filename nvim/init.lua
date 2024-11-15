bd3d2983575db6beb83e76de86d1aaae01bdc6fe
Justyn Temme <justyntemme@gmail.com>
2024-11-15 13:13:39 -0600

    use gb (go build) as a way to run files in buffer for both python and rust

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -24,27 +24,43 @@ vim.g.maplocalleader = "\\"
 -- Setup lazy.nvim
 require("lazy").setup({
 
-spec ={ -- add your plugins here
-	{"neovim/nvim-lspconfig"},
-	{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
-	{ import = "lazyvim.plugins.extras.lang.python" },
---	{"nvim-treesitter/nvim-treesitter"},
-	{ "catppuccin/nvim", 
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    		priority = 1000, -- make sure to load this before all the other start plugins
-    		config = function()
-      		-- load the colorscheme here
-      			vim.cmd([[colorscheme catppuccin-frappe]])
-    		end,
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  
+	spec = { -- add your plugins here
+		{ "neovim/nvim-lspconfig" },
+		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
+		{ import = "lazyvim.plugins.extras.lang.python" },
+		--	{"nvim-treesitter/nvim-treesitter"},
+		{
+			"catppuccin/nvim",
+			lazy = false, -- make sure we load this during startup if it is your main colorscheme
+			priority = 1000, -- make sure to load this before all the other start plugins
+			config = function()
+				-- load the colorscheme here
+				vim.cmd([[colorscheme catppuccin-frappe]])
+			end,
+		},
+		-- Configure any other settings here. See the documentation for more details.
+		-- colorscheme that will be used when installing plugins.
 
-  install = { colorscheme = { "catppuccin-frappe" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
-  highlight = { enabled = true},
-}
+		install = { colorscheme = { "catppuccin-frappe" } },
+		-- automatically check for plugin updates
+		checker = { enabled = true },
+		highlight = { enabled = true },
+	},
 })
-require'lspconfig'.pyright.setup{}
+-- Add custom LSP keybindings
+local lspconfig = require("lspconfig")
+
+-- Setup rust-analyzer with a keybinding for RustRun
+lspconfig.rust_analyzer.setup({
+	on_attach = function(_, bufnr)
+		-- Keybinding specific to Rust buffers
+		vim.keymap.set("n", "gb", "<cmd>RustRun<cr>", { buffer = bufnr })
+	end,
+})
+
+--require'lspconfig'.pyright.setup{}
+require("lspconfig").ruff.setup({
+	on_attach = function(client, bufnr)
+		vim.keymap.set("n", "gb", "<cmd>!python3 %<cr>", { buffer = bufnr })
+	end,
+})
