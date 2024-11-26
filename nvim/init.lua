fffe6a22248afd7f2810476a71a052dccfe948be
Justyn Temme <justyntemme@gmail.com>
2024-11-26 10:21:37 -0600

    add go build for golang shortcut

--- nvim/init.lua
+++ nvim/init.lua
@@ -30,16 +30,19 @@ require("lazy").setup({
 			extensions = {
 				fzf = {
 					fuzzy = true,
-					override_generic_sorter = true,
+					override_generic_sorter = true, "gosum",
 					override_file_sorter = true,
 					case_mode = "smart_case",
 				},
 			},
 		},
-		{ "neovim/nvim-lspconfig" },
+    {"williamboman/mason.nvim",
+    ops = { ensure_installed = {"goimports", "gofumpt"}},
 		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
 		{ import = "lazyvim.plugins.extras.lang.python" },
-		{ "nvim-treesitter/nvim-treesitter" },
+		{ import = "lazyvim.plugins.extras.lang.go" },
+		{ "nvim-treesitter/nvim-treesitter",
+    opts = {ensure_installed = {"go", "gomodifytags", "impl", "gomod", "gowork", "gosum"}},
 		{
 			"catppuccin/nvim",
 			lazy = false, -- make sure we load this during startup if it is your main colorscheme
@@ -56,6 +59,72 @@ require("lazy").setup({
 		-- automatically check for plugin updates
 		checker = { enabled = true },
 		highlight = { enabled = true },
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				gopls = {
+					settings = {
+						gopls = {
+							gofumpt = true,
+							codelenses = {
+								gc_details = false,
+								generate = true,
+								regenerate_cgo = true,
+								run_govulncheck = true,
+								test = true,
+								tidy = true,
+								upgrade_dependency = true,
+								vendor = true,
+							},
+							hints = {
+								assignVariableTypes = true,
+								compositeLiteralFields = true,
+								compositeLiteralTypes = true,
+								constantValues = true,
+								functionTypeParameters = true,
+								parameterNames = true,
+								rangeVariableTypes = true,
+							},
+							analyses = {
+								fieldalignment = true,
+								nilness = true,
+								unusedparams = true,
+								unusedwrite = true,
+								useany = true,
+							},
+							usePlaceholders = true,
+							completeUnimported = true,
+							staticcheck = true,
+							directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+							semanticTokens = true,
+						},
+					},
+				},
+			},
+			setup = {
+				gopls = function(_, opts)
+					-- workaround for gopls not supporting semanticTokensProvider
+					-- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+					LazyVim.lsp.on_attach(function(client, _)
+						if not client.server_capabilities.semanticTokensProvider then
+							local semantic = client.config.capabilities.textDocument.semanticTokens
+							client.server_capabilities.semanticTokensProvider = {
+								full = true,
+								legend = {
+									tokenTypes = semantic.tokenTypes,
+									tokenModifiers = semantic.tokenModifiers,
+								},
+								range = true,
+							}
+						end
+					end, "gopls")
+					-- end workaround
+				end,
+				vim.keymap.set("n", "gb", "<cmd>GoRun<cr>", { buffer = bufnr }),
+			},
+		},
 	},
 })
 -- Add custom LSP keybindings
