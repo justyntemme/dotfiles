bc8bcae5cdb4b43601abfb2c820da696e19f6f35
Justyn Temme <justyntemme@gmail.com>
2025-01-04 20:27:25 -0600

    Auto show on cursor hold

--- nvim/init.lua
+++ nvim/init.lua
@@ -67,6 +67,28 @@ require("lazy").setup({
 		highlight = { enabled = true },
 	},
 })
+vim.api.nvim_create_autocmd("CursorHold", {
+	pattern = "*",
+	callback = function()
+		for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+			if vim.api.nvim_win_get_config(winid).zindex then
+				return
+			end
+		end
+		vim.diagnostic.open_float({
+			scope = "cursor",
+			focusable = false,
+			close_events = {
+				"CursorMoved",
+				"CursorMovedI",
+				"BufHidden",
+				"InsertCharPre",
+				"WinLeave",
+			},
+		})
+	end,
+})
+buffnr = buffr
 vim.keymap.set("n", "<leader>H", "<cmd>lua vim.diagnostic.open_float()<cr>", {})
 -- Global Keybindings
 require("hardtime").setup()
