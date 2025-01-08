99842a444cfc695798d463dd887ed27d8d862b16
Justyn Temme <justyntemme@gmail.com>
2025-01-07 18:34:56 -0600

    let lazy manage plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -53,20 +53,6 @@ require("lazy").setup({
 			dependencies = { "MunifTanjim/nui.nvim" },
 			opts = {},
 		},
-		{
-			"ray-x/go.nvim",
-			dependencies = { -- optional packages
-				"ray-x/guihua.lua",
-				"neovim/nvim-lspconfig",
-				"nvim-treesitter/nvim-treesitter",
-			},
-			config = function()
-				require("go").setup()
-			end,
-			event = { "CmdlineEnter" },
-			ft = { "go", "gomod" },
-			build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-		},
 		{ "williamboman/mason.nvim", opts = {
 			ensure_installed = { "goimports", "gofumpt" },
 		} },
