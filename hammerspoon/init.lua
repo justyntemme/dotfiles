cb04681a8efe55ef5b706382ece8ba78ec2c0369
Justyn Temme <justyntemme@gmail.com>
2025-10-05 18:35:38 -0500

    using less perf vesrion works fine, but now its time to tinker

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -3,25 +3,37 @@ hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
 spoon.ReloadConfiguration:bindHotkeys({ reloadConfiguration = { { "cmd", "ctrl", "shift" }, "R" } })
--- Hot reloading of config on save >> Now a spoon
--- function reloadConfig(files)
---   doReload = false
---   for _,file in pairs(files) do
---       if file:sub(-4) == ".lua" then
---           doReload = true
---       end
---   end
---   if doReload then
---       hs.reload()
---   end
--- end
--- myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+
+-- Move Focus window up/down left/right & cycle for focus window
+hs.hotkey.bind({ "cmd", "shift" }, "9", "Left", function()
+	-- Always use false, false as the last two params, frontmost (dont ignore windows with other windows in front), strict
+	-- TODO improve window speed by giving it some windows
+	-- local ws = wf.new({ override = { visible = true, fullscreen = false, allowScreens = "-1,0", currentSpace = true } })
+	-- hs.window.filter.defaultCurrentSpace:focusWindowWest(nil, true, false)
+	-- hs.window.filter.focusWest(nil, true, false)
+	--hs.window.focusWindowWest(nil,self:getWindows(),)
+end)
+hs.hotkey.bind({ "cmd", "shift" }, "0", "Right", function()
+	-- hs.window.filter.focusEast(nil, true, false)
+
+	-- local ws = wf.new({ "TextEdit", "Sublime Text", "BBEdit" })
+	-- :setRegions(hs.screen.primaryScreen():fromUnitRect("0.5,0/1,1"))
+	hs.window.filter.defaultCurrentSpace:focusWindowEast(nil, true, false)
+end)
+
+hs.hotkey.bind({ "cmd" }, "9", "Down", function()
+	hs.window.filter.defaultCurrentSpace:focusWindowSouth(nil, true, false)
+	-- hs.window.focusSouth(nil, true, false)
+end)
+hs.hotkey.bind({ "cmd" }, "0", "Left", function()
+	hs.window.filter.defaultCurrentSpace:focusWindowNorth(nil, true, false)
+	-- hs.window.focusNorth(nil, true, false)
+end)
+
+-- TODO: cycle ability
+-- hs.hotkey.bind({ "cmd", "shift" }, "f", "Left", function()
+-- 	hs.window.filter.defaultCurrentSpace:focusWindowWest(nil, true, false)
+-- 	-- hs.window.focusWindowWest(nil, true, false)
+-- end)
+--
 -- hs.alert.show("Config loaded")
-
--- Old method of manual reloading >> kept for documentation purposes
-
--- hs.hotkey.bind({"cmd", "shift"}, "R", function()
---     hs.reload()
---   end)
--- hs.alert.show("Config loaded")
-
