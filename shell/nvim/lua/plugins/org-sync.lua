fbeb90a880ead31674f75501bf33ed8139d73f72
Justyn Temme <justyntemme@gmail.com>
2025-10-16 13:39:39 -0500

    add org-sync.nvim

---
+++ shell/nvim/lua/plugins/org-sync.lua
@@ -0,0 +1,13 @@
+return {
+  "justyntemme/org-sync.nvim",
+  -- The event trigger now lives here, in your personal config.
+  -- event = "VeryLazy",
+  -- event = { "BufReadPost ~/.org/**/*.org", "BufWritePost ~/.org/**/*.org" },
+  event = { "BufReadPost *.org", "BufWritePost *.org" },
+  -- The config function calls the `setup` function from your plugin.
+  config = function()
+    require("org-sync").setup({
+      dir = "~/.org",
+    })
+  end,
+}
