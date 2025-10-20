5046b71e2a2e471c99fecbec1f7d03608452525f
Justyn Temme <justyntemme@gmail.com>
2025-10-20 17:41:08 -0500

    add all files to agenda files

--- shell/nvim/lua/plugins/orgmode.lua
+++ shell/nvim/lua/plugins/orgmode.lua
@@ -4,8 +4,14 @@ return {
   config = function()
     -- Setup orgmode
     require("orgmode").setup({
-      org_agenda_files = "~/.config/org/orgfiles/**/*",
+      org_agenda_files = "~/.config/org/**/*.org",
       org_default_notes_file = "~/.config/org/orgfiles/refile.org",
+      mappings = {
+        global = {
+          -- org_agenda = true,
+          org_capture = "gC",
+        },
+      },
     })
   end,
 }
