dd121b03b9d7f8c65defc61ef58294fa281f414d
Justyn Temme <justyntemme@gmail.com>
2025-10-20 18:14:31 -0500

    use org-modern for ui elements (eventually lets move to snacks)

--- shell/nvim/lua/plugins/orgmode.lua
+++ shell/nvim/lua/plugins/orgmode.lua
@@ -1,11 +1,33 @@
 return {
   "nvim-orgmode/orgmode",
+  dependencies = {
+    "danilshvalov/org-modern.nvim",
+  },
   event = "VeryLazy",
   config = function()
-    -- Setup orgmode
+    local Menu = require("org-modern.menu")
+
     require("orgmode").setup({
       org_agenda_files = "~/.config/org/**/*.org",
       org_default_notes_file = "~/.config/org/orgfiles/refile.org",
+      ui = {
+        menu = {
+          handler = function(data)
+            Menu:new({
+              window = {
+                margin = { 1, 0, 1, 0 },
+                padding = { 0, 1, 0, 1 },
+                title_pos = "center",
+                border = "single",
+                zindex = 1000,
+              },
+              icons = {
+                separator = "➜",
+              },
+            }):open(data)
+          end,
+        },
+      },
       mappings = {
         global = {
           -- org_agenda = true,
