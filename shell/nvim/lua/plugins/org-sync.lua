3e418d6fd176ff20b236b8254723414aeddbe886
Justyn Temme <justyntemme@gmail.com>
2026-01-11 14:34:08 -0600

    Only sync on files in specific directory

--- shell/nvim/lua/plugins/org-sync.lua
+++ shell/nvim/lua/plugins/org-sync.lua
@@ -3,7 +3,7 @@ return {
   -- The event trigger now lives here, in your personal config.
   -- event = "VeryLazy",
   -- event = { "BufReadPost ~/.org/**/*.org", "BufWritePost ~/.org/**/*.org" },
-  event = { "BufReadPost *.org", "BufWritePost *.org" },
+  event = { "BufReadPost ~/.config/org/**/*.org", "BufWritePost ~/.config/org/**/*.org" },
   -- The config function calls the `setup` function from your plugin.
   config = function()
     require("org-sync").setup({
